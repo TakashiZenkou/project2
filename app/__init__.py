2f23db343e08f64f141490e90376c367a6775be1
Jason Wan <jaysun1w@gmail.com>
2022-04-03 22:10:34 -0500

    Models added

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from .config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+csrf = CSRFProtect(app)
+db = SQLAlchemy(app)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 
 from app import views
+
+# Instantiate Flask-Migrate library here
+migrate = Migrate(app,db)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,121 @@
-# Add any model classes for Flask-SQLAlchemy here
+from . import db
+from werkzeug.security import generate_password_hash
+
+class Cars(db.model):
+
+    __tablename__ = 'Cars'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100))
+    make = db.Column(db.String(100))
+    model = db.Column(db.String(100))
+    colour = db.Column(db.String(100))
+    year = db.Column(db.String(100))
+    transmission = db.Column(db.String(100))
+    car_type = db.Column(db.String(100))
+    price = db.Column(db.Float(10,2))
+    photo = db.Column(db.String(100))
+    user_id = db.Column(db.Integer)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+    def __init__(self,description,make,model,colour,year,transmission,car_type,price,photo,user_id):
+        self.description = description
+        self.make = make
+        self.model = model
+        self.colour = colour
+        self.year = year
+        self.transmission = transmission
+        self.car_type = car_type
+        self.price = price
+        self.photo = photo
+        self.user_id = user_id
+
+class Favourites(db.model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    car_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+    def __init__(self,car_id,user_id):
+        self.car_id = id
+        self.user_id = user_id
+
+class Users(db.model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100),unique = True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    location = db.Column(db.String(100))
+    biography = db.Column(db.String(100))
+    photo = db.Column(db.String(100))
+    date_joined = db.Column(db.DateTime)	
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+    def __init__(self,username,password,name,email,location,biography,photo,date_joined):
+        self.username = username
+        self.password = generate_password_hash(password,method='pbkdf2:sha256')
+        self.name = name
+        self.email = email
+        self.location = location
+        self.biography = biography
+        self.photo = photo
+        self.date_joined = date_joined
+
+
+
+
+
+
