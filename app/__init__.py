6422524e8092c8463dc5d32f09d07fa4ee9916cf
Kieranator5050 <thejugg63@gmail.com>
2022-04-23 23:11:46 -0500

    a

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_migrate import Migrate
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../dist/assets')
 app.config.from_object(Config)
 app.config['JSON_SORT_KEYS'] = False
 csrf = CSRFProtect(app)
@@ -21,5 +21,3 @@ login_manager.login_view = 'login'
 migrate = Migrate(app,db)
 
 from app import views
-
-app = Flask(__name__, static_folder='../dist/assets')
