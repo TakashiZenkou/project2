dad828d3afee84a09efab9d0d66438dc763bac77
Kieranator5050 <thejugg63@gmail.com>
2022-04-24 11:15:35 -0500

    e

--- app/views.py
+++ app/views.py
@@ -158,8 +158,7 @@ def pcars():
                 picture = request.files['photo']
                 filename = secure_filename(picture.filename)
 
-                mydir = os.path.dirname(__file__)
-                picture.save(mydir,os.path.join(app.config['UPLOAD_FOLDER'],filename))
+                picture.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
                 
                 user_id = request.form['user_id']
                 car = Cars(description,make,model,color,year,transmission,cartype,price,filename,user_id)
